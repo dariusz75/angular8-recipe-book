52c5e363a733b60b9762e86df82f933af846da51
Dariusz Franczak <darius32@op.pl>
2020-01-06 23:57:28 +0000

    scss issue fixed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,7 +6,7 @@ import { from } from 'rxjs';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.sass']
+  styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
 
